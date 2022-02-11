dc9f34ea3077076d23171e58e23f3dca9a950d2a
quentin <bm.quentin@icloud.com>
2022-02-11 17:57:36 +0100

    save map

--- Le_jeu_de_la_vie/Assets/Script/SaveGame.cs
+++ Le_jeu_de_la_vie/Assets/Script/SaveGame.cs
@@ -1,12 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class SaveGame : MonoBehaviour
 {
 
-    [SerializeField]public Button button_save;
+    [SerializeField] public Button button_save;
+    [SerializeField] private TMP_InputField _NameFile;
+    private string path;
+    private string line;
 
     private void Start()
     {
@@ -21,6 +24,35 @@ public class SaveGame : MonoBehaviour
 
     private void TaskOnClickSave()
     {
+        try
+        {
+            path = "./Assets/save/" + _NameFile.text + ".txt";
+            path = Path.GetFullPath(path);
+            Debug.Log(path);
+            StreamWriter file = new StreamWriter(path);
+            for (int i = 0; i < CreateMap.Instance._ColX; i++)
+            {
+                line = "";
+                for (int j = 0; j < CreateMap.Instance._ColY; j++)
+                {
+                    if (CreateMap.Instance._Grid[i,j].GetComponent<SpriteRenderer>().color == Color.white)
+                    {
+                        line += "1";
+                    }
+                    else
+                    {
+                        line += "0";
+                    }
+                }
+                file.WriteLine(line);
+            }
+            file.Close();
+
+        }
+        finally
+        {
+            Debug.Log("probleme save !");
+        }
         Debug.Log("button save");
     }
 }
