9826cc6462a1a010e4a046e18d3634e10e07672c
quentin <bm.quentin@icloud.com>
2022-02-15 11:25:04 +0100

    change size map

--- Le_jeu_de_la_vie/Assets/Script/CreateMap.cs
+++ Le_jeu_de_la_vie/Assets/Script/CreateMap.cs
@@ -9,6 +9,7 @@ public class CreateMap : MonoBehaviour
     public GameObject prefab;
 
     public GameObject[,] _Grid;
+    private GameObject[,] _GridTemp;
 
     public Slider sliderUI;
 
@@ -20,7 +21,12 @@ public class CreateMap : MonoBehaviour
 
     private void Awake()
     {
+        CreateFirstMap(); 
+    }
 
+
+    private void CreateFirstMap()
+    {
         if (Instance == null)
         {
             Instance = this;
@@ -39,17 +45,61 @@ public class CreateMap : MonoBehaviour
                 Vector3 pos = new Vector3(i + 0.5f, j + 0.5f, 0);
                 GameObject clone = Instantiate(prefab, pos, Quaternion.identity);
                 _Grid[i, j] = clone;
-                
+
             }
         }
-        
     }
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeSizeMap(int x, int y)
     {
-        
+        _GridTemp = new GameObject[x, y];
+
+        if (x < _ColX)
+        {
+            ReductMap(x, y);
+        }
+        else if (x > _ColX)
+        {
+            ExpendMap(x, y);
+        }
+        else
+        {
+            Debug.Log("same size");
+        }
     }
 
+    private void ReductMap(int x, int y)
+    {
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                _GridTemp[i, j] = _Grid[i, j];
+            }
+        }
+        _Grid = new GameObject[x, y];
+        _Grid = _GridTemp;
+    }
 
-
+    private void ExpendMap(int x, int y)
+    {
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                if (i >= _ColX || j >= _ColY)
+                {
+                    Vector3 pos = new Vector3(i + 0.5f, j + 0.5f, 0);
+                    GameObject clone = Instantiate(prefab, pos, Quaternion.identity);
+                    _GridTemp[i, j] = clone;
+                }
+                else
+                {
+                    _GridTemp[i, j] = _Grid[i, j];
+                }
+            }
+        }
+        _Grid = new GameObject[x, y];
+        _Grid = _GridTemp;
+    }
+    
 }
